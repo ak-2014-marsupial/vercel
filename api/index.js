93ec0276a1f311dda9880f26b98b599e8b8ba8c5
Andrew <ak36794ca@gmail.com>
2024-11-12 11:23:03 +0200

    init

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors")
+
+dotenv.config();
+const port = process.env.API_PORT;
+app.use(cors("http://localhost:4000/api"))
+
+app.get("/api/test", (req, res) => {
+    res.json(`Hello World ${Date.now()}`)
+})
+
+if (port) {
+    app.listen(port)
+    console.log(`Server has started on port ${port}`);
+}
+
+module.exports=app
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [result, setResult] = useState("")
+    const fetchInfo = async () => {
+        const url = `${process.env.REACT_APP_API_URL}/test`;
+        const response = await fetch(url);
+        const json = await response.json();
+        setResult(json)
+        console.log(json);
+    }
+    return (
+        <div>
+            <button onClick={fetchInfo}>fetch info from Vercel</button>
+            <div>result:{result}</div>
+        </div>
+    );
 }
 
 export default App;
