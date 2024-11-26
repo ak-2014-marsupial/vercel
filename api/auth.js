0067cb85d3f0f9b8df6a85dc3842199a932975e2
Andrew <ak36794ca@gmail.com>
2024-11-26 20:39:50 +0200

    fixed CORS

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,18 @@ import connectDB from "../src_backend/configs/connectionDB";
 import {getHandler} from "../src_backend/routes";
 
 export default async function handler(req, res) {
-    handlerCORS(req, res);
+    // handlerCORS(req, res);
+
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.status(200).end(); // Respond to preflight requests
+        return;
+    }
+
     connectDB().then();
 
     const fullUrl = req.url;
