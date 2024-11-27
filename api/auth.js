557f2b90f5fe7fca2e37146185e2c0bfd12c65b9
Andrew <ak36794ca@gmail.com>
2024-11-27 12:51:58 +0200

    returned it backЭ

--- api/auth.js
+++ api/auth.js
@@ -1,14 +1,18 @@
 import connectDB from "../src_backend/configs/connectionDB";
 import {getHandler} from "../src_backend/routes";
-import {handlerCORS} from "../src_backend/handlers/cors.handler";
 
 export default async function handler(req, res) {
-    handlerCORS(req, res);
+
+
+    console.log('Request Origin:', origin);
+    res.setHeader('Access-Control-Allow-Origin', 'https://ann-227.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     // Handle preflight requests
     if (req.method === 'OPTIONS') {
         res.status(200).end();
-        // console.log('CORS preflight response sent with status 200');
+        console.log('CORS preflight response sent with status 200');
         return;
     }
 
