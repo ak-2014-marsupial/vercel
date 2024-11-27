77e1a023491af28e595c9f58c51f895fcaf7830b
Andrew <ak36794ca@gmail.com>
2024-11-27 13:06:37 +0200

    fixed 5

--- api/auth.js
+++ api/auth.js
@@ -4,15 +4,21 @@ import {getHandler} from "../src_backend/routes";
 export default async function handler(req, res) {
 
     const origin = req.headers.origin;
-    console.log('Request Origin:', origin);
-    res.setHeader('Access-Control-Allow-Origin', 'https://ann-227.github.io');
+    const allowedOrigins = process.env?.ALLOWED_ORIGINS?.split(",") || []
+    console.log({origin, allowedOrigins});
+    if(allowedOrigins.includes(origin)){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else{
+        res.setHeader('Access-Control-Allow-Origin', "");
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     // Handle preflight requests
     if (req.method === 'OPTIONS') {
         res.status(200).end();
-        console.log('CORS preflight response sent with status 200');
+        // console.log('CORS preflight response sent with status 200');
         return;
     }
 
